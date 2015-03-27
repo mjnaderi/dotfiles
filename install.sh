2ef4062f4444aa70f481710951d0bb3034b8392a
Mohammad Javad Naderi <mjnaderi@gmail.com>
2015-03-27 15:08:52 +0430

    update install.sh

--- install.sh
+++ install.sh
@@ -4,15 +4,19 @@ git submodule init
 git submodule update
 
 # Install Prezto
-echo -e "\n\nInstalling Prezto\n"
+echo -e "\n\n======= Installing Prezto\n"
 cd prezto
+echo -e "\n\n------- Cloning Prezto Submodules\n"
 git submodule init
 git submodule update
+echo -e "\n\n------- Creating symbolic links for Prezto\n"
 CURRENT_DIR=`pwd`
 setopt EXTENDED_GLOB
+ln -svf prezto "${ZDOTDIR:-$HOME}/.zprezto"
 for rcfile in "$CURRENT_DIR"/runcoms/^README.md(.N); do
     ln -svf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 cd ..
+
 
 
