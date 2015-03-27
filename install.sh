2c988a3d3bcb60d0200c519f9509f7ee9f0dcdc3
Mohammad Javad Naderi <mjnaderi@gmail.com>
2015-03-27 14:32:50 +0430

    update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,14 @@
 
 CURRENT_DIR=`pwd`
 
+git submodule init
+git submodule update
+
+cd prezto
+git submodule init
+git submodule update
+cd ..
+
 setopt EXTENDED_GLOB
 for rcfile in "$CURRENT_DIR"/prezto/runcoms/^README.md(.N); do
     ln -svf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
