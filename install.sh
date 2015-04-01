07159f95d9b3593c88d9118e4cd3c7cbf44b5c1a
Mohammad Javad Naderi <mjnaderi@gmail.com>
2015-04-01 15:11:30 +0430

    add base16-shell

--- install.sh
+++ install.sh
@@ -12,11 +12,16 @@ git submodule init
 git submodule update
 echo -e "\n\n------- Creating symbolic links for Prezto\n"
 setopt EXTENDED_GLOB
+rm -rf "${ZDOTDIR:-$HOME}/.zprezto"
 ln -svf $PREZTO_DIR "${ZDOTDIR:-$HOME}/.zprezto"
 for rcfile in "$PREZTO_DIR"/runcoms/^README.md(.N); do
     ln -svf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 cd ..
+
+# Install base16-shell
+rm -rf "${ZDOTDIR:-$HOME}/.base16-shell"
+ln -svf `pwd`"/base16-shell" "${ZDOTDIR:-$HOME}/.base16-shell"
 
 
 
