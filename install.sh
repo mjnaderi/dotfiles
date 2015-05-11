f88448d89bb0ad7a00a8721caacfb8cd42ffeb59
Mohammad Javad Naderi <mjnaderi@gmail.com>
2015-05-12 01:55:08 +0430

    Add --no-dereference option to ln\n solves http://superuser.com/questions/645842/how-to-overwrite-symblic-link-of-directory

--- install.sh
+++ install.sh
@@ -17,23 +17,23 @@ git submodule update
 echo -e "\n\n------- Creating symbolic links for Prezto\n"
 setopt EXTENDED_GLOB
 rm -rf "${ZDOTDIR:-$HOME}/.zprezto"
-ln -svf $PREZTO_DIR "${ZDOTDIR:-$HOME}/.zprezto"
+ln -svfn $PREZTO_DIR "${ZDOTDIR:-$HOME}/.zprezto"
 for rcfile in "$PREZTO_DIR"/runcoms/^README.md(.N); do
-    ln -svf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    ln -svfn "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 cd ..
 
 # Install base16-shell
 echo -e "\n\n======= Installing Base16-Shell\n"
 rm -rf "${ZDOTDIR:-$HOME}/.base16-shell"
-ln -svf `pwd`"/base16-shell" "${ZDOTDIR:-$HOME}/.base16-shell"
+ln -svfn `pwd`"/base16-shell" "${ZDOTDIR:-$HOME}/.base16-shell"
 
 # Install vimrc and Vundle
 echo -e "\n\n======= Installing vimrc and Vundle\n"
 mkdir -p "${ZDOTDIR:-$HOME}/.vim/bundle"
-ln -svf `pwd`"/vundle" "${ZDOTDIR:-$HOME}/.vim/bundle/Vundle.vim"
-ln -svf `pwd`"/vimrc" "${ZDOTDIR:-$HOME}/.vimrc"
-ln -svf `pwd`"/vundlerc" "${ZDOTDIR:-$HOME}/.vundlerc"
+ln -svfn `pwd`"/vundle" "${ZDOTDIR:-$HOME}/.vim/bundle/Vundle.vim"
+ln -svfn `pwd`"/vimrc" "${ZDOTDIR:-$HOME}/.vimrc"
+ln -svfn `pwd`"/vundlerc" "${ZDOTDIR:-$HOME}/.vundlerc"
 echo -e "\n\n------- Installing Vundle packages\n"
 source <(echo "vim +BundleInstall +qall -u vundlerc")
 
